9162e6dd6da2365006b20ae0b5f328ec0c78abbc
Ken Tominaga <ken.for.develop@gmail.com>
2014-11-08 15:18:06 +0900

    Add files

--- RomoSwift/RomoSwift-Bridging-Header.h
+++ RomoSwift/RomoSwift-Bridging-Header.h
@@ -2,3 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#undef I
+#define I II // some substitution of I (like II, In, ∫)
+#undef I
+
+#import <RMCharacter/RMCharacter.h>
+#import <RMCore/RMCore.h>
--- RomoSwift/ViewController.swift
+++ RomoSwift/ViewController.swift
@@ -8,16 +8,110 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RMCoreDelegate{
 
+    var romo: RMCharacter?
+    var robot: RMCoreRobotRomo3?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        romo = RMCharacter.Romo()
+        romo?.expression = RMCharacterExpressionCurious
+        romo?.emotion = RMCharacterEmotionScared
+        romo?.lookAtPoint(RMPoint3D(x: -1.0, y: -1.0, z: 0.5), animated: true)
+        
+        RMCore.setDelegate(self)
+        
+        // check
+        let con = RMCoreControllerPID()
+        con.I = 5
+        println(con.I)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        romo?.addToSuperview(self.view)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        romo?.removeFromSuperview()
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        let touchLocation = touches.anyObject()?.locationInView(self.view)
+        lookAtPoint(touchLocation!)
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touchLocation = touches.anyObject()?.locationInView(self.view)
+        lookAtPoint(touchLocation!)
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        romo?.lookAtDefault()
+        
+        let numberOfExpressions: UInt32 = 19
+        let numberOfEmotions: UInt32 = 7
+        
+        let randomExpression = RMCharacterExpression(arc4random_uniform(numberOfExpressions) + 1)
+        let randomEmotion = RMCharacterEmotion(arc4random_uniform(numberOfExpressions) + 1)
+        
+        romo?.setExpression(randomExpression, withEmotion: randomEmotion)
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        romo?.lookAtDefault()
+    }
+    
+    func lookAtPoint(touchLocation: CGPoint) {
+        // Maxiumum distance from the center of the screen = half the width
+        let w_2 = self.view.frame.size.width / 2
+        
+        // Maximum distance from the middle of the screen = half the height
+        let h_2 = self.view.frame.size.height / 2
+        
+        // Ratio of horizontal location from center
+        let x = (touchLocation.x - w_2) / w_2
+        
+        // Ratio of vertical location from middle
+        let y = (touchLocation.y - h_2) / h_2
+        
+        // Since the touches are on Romo's face, they
+        let z: CGFloat = 0.0
+        
+        // Romo expects a 3D point
+        // x and y between -1 and 1, z between 0 and 1
+        // z controls how far the eyes diverge
+        // (z = 0 makes the eyes converge, z = 1 makes the eyes parallel)
+        let lookPoint = RMPoint3D(x: x, y: y, z: z)
+        
+        // Tell Romo to look at the point
+        // We don't animate because lookAtTouchLocation: runs at many Hertz
+        romo?.lookAtPoint(lookPoint, animated: false)
+    }
+    
+    // MARK: RMCoreDelegate
+    
+    func robotDidConnect(robot: RMCoreRobot!) {
+        if robot.drivable && robot.headTiltable && robot.LEDEquipped {
+            self.robot = robot as? RMCoreRobotRomo3
+        }
+    }
+    
+    func robotDidDisconnect(robot: RMCoreRobot!) {
+        if robot == self.robot {
+            self.robot = nil;
+        }
     }
 
 
